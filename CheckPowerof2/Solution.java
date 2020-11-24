f2bf81c035608ad5ab489d9287d11ec2b61bf50f
Swathi Penmatsa <74686794+swathipenmatsa19@users.noreply.github.com>
2020-11-24 14:45:18 -0600

    Check Power of 2

---
+++ CheckPowerof2/Solution.java
@@ -0,0 +1,78 @@
+/*
+
+
+
+Given a number, check if it is a power of 2. 
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line containing a single positive integer. 
+
+Constraints
+
+1 <= T <= 10000 
+1 <= N <= 1018 
+
+Output Format
+
+For each test case, print "True" or "False", separated by new line. 
+
+Sample Input 0
+
+5
+1
+8
+10
+25
+512
+Sample Output 0
+
+True
+True
+False
+False
+True
+
+
+
+*/
+
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static boolean isPowerof2(long n) {
+        if((n & (n - 1)) == 0)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- != 0) {
+            long n = sc.nextLong();
+            if(isPowerof2(n))
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
